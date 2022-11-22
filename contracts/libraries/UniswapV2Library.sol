cb0cbe8f4d8041edb42ff4e4e3b86c02867f4a70
Ivaylo Petrov <isivaylopetrov@gmail.com>
2022-11-22 09:03:33 +0200

    use hydra testnet v2 init code hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -28,7 +28,7 @@ library UniswapV2Library {
                         hex'ff',
                         factory,
                         keccak256(abi.encodePacked(token0, token1)),
-                        hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                        hex'00f5a7aa52ba04ca4b2f7f2ef08b82a61ac4e8fead11ebd9c4fbc48edc95b708' // init code hash (HYDRA TESTNET)
                     )
                 )
             )
